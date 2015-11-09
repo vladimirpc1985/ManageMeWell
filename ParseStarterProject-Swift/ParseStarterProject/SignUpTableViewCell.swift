f527ef3c56f3e0008fcbd905835b4acb1f9c4866
vladimirpc1985 <vladimirpc1985@gmail.com>
2015-11-09 16:04:48 -0500

    Change in Login and Sign Up

---
+++ ParseStarterProject-Swift/ParseStarterProject/SignUpTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpTableViewCell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Lesly Villa on 11/6/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignUpTableViewCell: UITableViewCell
+{
+    
+    @IBOutlet weak var informationField: UITextField!
+    
+}
